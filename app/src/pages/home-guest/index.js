bc3636a2b3672934404484a8d3bed3d1544b085f
vasilzahariev <vasilzahariev1707@abv.bg>
2020-08-16 09:17:02 +0300

    fix: Guest Home page is now looking better

--- app/src/pages/home-guest/index.js
+++ app/src/pages/home-guest/index.js
@@ -5,27 +5,35 @@ import {
     Link
 } from 'react-router-dom';
 import styles from './index.module.css';
+import Grid from '@material-ui/core/Grid';
 
 const HomeGuest = () => {
     return (
         <Layout>
             <div className={styles.cards}>
-                <HomePageCard>
-                    <div className={styles.signUpText} >Join us, we've got <span className={styles.epicGamer}>games</span>!</div>
-                    <Link className={styles.signUpLink} to='/register'>REGISTER</Link>
-                </HomePageCard>
-
-                <HomePageCard>
-                    Write <span className={styles.epicGamer}>reviews</span> of your <span>favorite games</span> and add them to <span>custom lists</span>
-                </HomePageCard>
-
-                <HomePageCard>
-                    Find other people <span className={styles.epicGamer}>(friends and etc)</span> and follow them to see their <span>activity</span>
-                </HomePageCard>
-
-                <HomePageCard>
-                    Keep track of the games <span className={styles.epicGamer}>you've played</span> and <span>want to play</span>
-                </HomePageCard>
+                <Grid container direction='row' justify='center' alignItems='flex-start' spacing={10} >
+                    <Grid item xs={5}>
+                        <HomePageCard>
+                            <div className={styles.signUpText} >Join us, we've got <span className={styles.epicGamer}>games</span>!</div>
+                            <Link className={styles.signUpLink} to='/register'>REGISTER</Link>
+                        </HomePageCard>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <HomePageCard>
+                            Write <span className={styles.epicGamer}>reviews</span> of your <span>favorite games</span> and add them to <span>custom lists</span>
+                        </HomePageCard>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <HomePageCard>
+                            Find other people <span className={styles.epicGamer}>(friends and etc)</span> and follow them to see their <span className={styles.epicGamer}>activity</span>
+                        </HomePageCard>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <HomePageCard>
+                            Keep track of the games <span className={styles.epicGamer}>you've played</span> and <span className={styles.epicGamer}>want to play</span>
+                        </HomePageCard>
+                    </Grid>
+                </Grid>
             </div>
         </Layout>
     );
